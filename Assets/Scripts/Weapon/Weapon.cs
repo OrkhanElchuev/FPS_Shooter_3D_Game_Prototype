f0a1ecc71135c75bb7dfa1a9f9c1a543cf1d51d8
OrkhanElchuev <orkhan.elchuev@gmail.com>
2026-01-20 02:08:14 +0100

    Shoot Through Pickable items

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -1,16 +1,19 @@
+using System;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] LayerMask interactionLayers;
 
     public void Shoot(WeaponSO weaponSO)
     {
         RaycastHit hit;
         muzzleFlash.Play();
 
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, 
+            out hit, Mathf.Infinity, interactionLayers, QueryTriggerInteraction.Ignore))
         {
             // Create shoot particle effect on the surface that the ray hits
             Instantiate(weaponSO.HitVFXPrefab, hit.point, Quaternion.identity);
